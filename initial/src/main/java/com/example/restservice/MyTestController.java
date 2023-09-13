db7a372832c221379d472915d38845bcbe055e8d
Vlad Moshkov <moshkovvlad70@gmail.com>
2023-09-13 21:01:51 +0300

    added dynamic html content in TestController2

--- initial/src/main/java/com/example/restservice/MyTestController.java
+++ initial/src/main/java/com/example/restservice/MyTestController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 
 //import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
