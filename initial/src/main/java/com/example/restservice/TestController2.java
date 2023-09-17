d1c91b4049eb010905ba4f50257f121e5e29a181
Vlad Moshkov <moshkovvlad70@gmail.com>
2023-09-17 19:23:53 +0300

    clear unnecessary files

--- initial/src/main/java/com/example/restservice/TestController2.java
+++ initial/src/main/java/com/example/restservice/TestController2.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TestController2 {
 
-	@GetMapping("/greeting2")
+	@GetMapping("/greeting")
     //name in format http://localhost:8080/greeting?name=User
 	public String greeting2(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
-		return "greeting2";
+		return "greeting";
 	}
 
 }
